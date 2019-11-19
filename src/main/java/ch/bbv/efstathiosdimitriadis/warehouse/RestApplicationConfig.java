d8619fbb318f852459e594ff986a809a9148e46f
Efstathios Dimitriadis <efstathios.dimitriadis@bbv.ch>
2019-11-19 15:02:48 +0200

    Setup RESTEasy

---
+++ src/main/java/ch/bbv/efstathiosdimitriadis/warehouse/RestApplicationConfig.java
@@ -0,0 +1,9 @@
+package ch.bbv.efstathiosdimitriadis.warehouse;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplicationConfig extends Application {
+
+}
