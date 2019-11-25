38050ae89097963096c61c856cdb92686e446c1e
Efstathios Dimitriadis <efstathiosdimitriadis@bbv.ch>
2019-11-25 12:21:40 +0200

    Remove unused annotation from Product

--- src/main/java/ch/bbv/efstathiosdimitriadis/rest/model/Product.java
+++ src/main/java/ch/bbv/efstathiosdimitriadis/rest/model/Product.java
@@ -3,16 +3,8 @@ package ch.bbv.efstathiosdimitriadis.rest.model;
 import java.io.Serializable;
 import java.util.UUID;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
