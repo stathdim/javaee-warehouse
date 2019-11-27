91864c5211cef9a32afba998d68cc65bf1529363
Efstathios Dimitriadis <efstathiosdimitriadis@bbv.ch>
2019-11-27 13:24:56 +0200

    Delegate GET request at /products to ProductService

--- src/main/java/ch/bbv/efstathiosdimitriadis/rest/resource/ProductResource.java
+++ src/main/java/ch/bbv/efstathiosdimitriadis/rest/resource/ProductResource.java
@@ -1,5 +1,6 @@
 package ch.bbv.efstathiosdimitriadis.rest.resource;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,16 +9,30 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import ch.bbv.efstathiosdimitriadis.rest.model.Product;
-import ch.bbv.efstathiosdimitriadis.rest.model.ProductCategory;
+import ch.bbv.efstathiosdimitriadis.rest.service.ProductService;
+
 
 @Path("products")
 public class ProductResource {
+	private ProductService productService;
 	
+	public ProductResource() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Inject
+	public ProductResource(ProductService productService) {
+		this.productService = productService;
+	}
+
+
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getProduct() {
-		Product product = new Product("tire", ProductCategory.CAR_ACCESSORY);
-		return Response.ok(product).build();
+	public Response getProduct() throws Exception {
+		
+		Response productResponse = productService.getAll();
+		return productResponse;
 	}
 	
 	@POST
