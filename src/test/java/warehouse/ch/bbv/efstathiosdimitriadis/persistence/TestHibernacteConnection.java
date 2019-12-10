9fab8b703cacd73a97824e02928bb15877167f46
Efstathios Dimitriadis <efstathios.dimitriadis@bbv.ch>
2019-12-10 12:38:29 +0200

    Connect to different MSSQL dbs for testing and dev

--- src/test/java/warehouse/ch/bbv/efstathiosdimitriadis/persistence/TestHibernacteConnection.java
+++ src/test/java/warehouse/ch/bbv/efstathiosdimitriadis/persistence/TestHibernacteConnection.java
@@ -1,18 +1,42 @@
 package warehouse.ch.bbv.efstathiosdimitriadis.persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestReporter;
 
 import ch.bbv.efstathiosdimitriadis.rest.model.Product;
 import ch.bbv.efstathiosdimitriadis.rest.model.ProductCategory;
 
 public class TestHibernacteConnection {
+	
+	@Test 
+	void testMsSql() {
+		String connectionUrl = "jdbc:sqlserver://localhost;databaseName=warehouse;user=sa;password=sompa2019!";
+
+        try {
+            // Load SQL Server JDBC driver and establish connection.
+            System.out.print("Connecting to SQL Server ... ");
+            try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+                System.out.println("Done.");
+            }
+        } catch (Exception e) {
+            System.out.println();
+            e.printStackTrace();
+            fail();
+        }
+	}
 
 	@Test
 	void testConnection() {
@@ -29,17 +53,17 @@ public class TestHibernacteConnection {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("warehouse-persistence");
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
-		em.getTransaction().commit();
-		Product[] stored = {new Product("Kia Picanto", ProductCategory.CAR, 2019), new Product("Uni V7 Red", ProductCategory.STATIONERY, 2018)};
+		
+		Product[] stored = { new Product("Kia SPicantos", ProductCategory.CAR, 2019),
+				new Product("Uni V7 BReds", ProductCategory.STATIONERY, 2018) };
 		em.persist(stored[0]);
 		em.persist(stored[1]);
 		Product retrieved = em.find(Product.class, stored[0].getId());
-		
+		em.getTransaction().commit();
 		assertEquals(stored[0], retrieved);
 		
 		em.close();
 		emf.close();
 	}
-	
-	
+
 }
